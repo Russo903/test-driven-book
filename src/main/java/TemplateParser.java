09d84f3a470d803ed348bd8ca0d35e79a304083b
Gino Russo <grusso215@gmail.com>
2026-02-18 21:28:30 -0500

    Add TemplateParser and its initial tests
    Introduced the `TemplateParser` class with a basic `parse` method returning a list of template segments. Added corresponding test class `TestTemplateParse` with initial tests ensuring plain text and empty templates are correctly parsed.

---
+++ src/main/java/TemplateParser.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class TemplateParser {
+    
+    public TemplateParser() {
+    
+    }
+    
+    public List<String> parse(String template) {
+        return List.of(template);
+    }
+}
---
+++ src/test/java/TestTemplateParse.java
@@ -0,0 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestTemplateParse {
+    
+    @Test
+    public void emptyTemplateRendersEmptyString() {
+        List<String> segments = parse("");
+        assertSegments(segments, "");
+    }
+    
+    @Test
+    public void plainTextRendersPlainText() {
+        List<String> segments = parse("Plain text");
+        assertSegments(segments, "Plain text");
+    }
+    
+    private List<String> parse(String template) {
+        return new TemplateParser().parse(template);
+    }
+    
+    /**
+     * In our tests we can asserting these two things for the parse
+     *
+     * <pre>{@code
+     * assertEquals("Amount of segments", 1, segments.size());
+     * assertEquals("Plain Text String produced", "Plain text", segments.getFirst());
+     * }</pre>
+     *
+     * We can extract this into its own method to make this simple and make
+     * both assertions for us since they are related
+     * <p>
+     * Using varargs allows us to pass in multiple values and they will get collected into
+     * an array for us.
+     */
+    private void assertSegments(List<?> actual, Object... expected) {
+        assertEquals("Amount of segments", expected.length, actual.size());
+        assertEquals("compare contents", List.of(expected), actual);
+    }
+}
